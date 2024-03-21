369784810df2ffecd447d0ff8b43933b958be8fd
朱思楠 <zsn990205@163.com>
2024-03-21 15:41:38 +0800

    克隆

---
+++ Test_item.java
@@ -0,0 +1,52 @@
+package demo3;
+class Money implements Cloneable {
+    public double m = 19.9;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+
+class Person implements Cloneable{
+    public String name;
+    public int age;
+
+    Money money = new Money();
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+
+        Person tmp = (Person)super.clone();
+        tmp.money = (Money)this.money.clone();
+        return tmp;
+        //刚开始这里只克隆person了没看到克隆money
+        //return super.clone();
+    }
+}
+
+public class Test_item {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Person person = new Person("张三",10);
+        Person person2 = (Person)person.clone();
+
+        System.out.println("person: "+person.money.m);
+        System.out.println("person2: "+person2.money.m);
+        System.out.println("======================");
+        person.money.m = 99.9;
+        System.out.println("person: "+person.money.m);
+        System.out.println("person2: "+person2.money.m);
+    }
+}
