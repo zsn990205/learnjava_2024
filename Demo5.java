664a580eb1f7a98652ddf1596d1752e6397aa0ee
朱思楠 <zsn990205@163.com>
2024-05-28 14:32:32 +0800

    线程创建方式5

---
+++ Demo5.java
@@ -0,0 +1,25 @@
+package Thread;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            while(true) {
+                System.out.println("hello thread");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+        while(true) {
+            System.out.println("hello main");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
