63a02d49d513b582924c752fd57b1aa82341b665
朱思楠 <zsn990205@163.com>
2024-06-14 14:31:41 +0800

    线程

---
+++ Demo16.java
@@ -0,0 +1,16 @@
+package Thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Demo16 {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        service.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello");
+            }
+        });
+    }
+}
