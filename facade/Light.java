8d50fc35ea47d352d55957619f49a50c9291828f
朱思楠 <zsn990205@163.com>
2024-12-06 16:46:27 +0800

    demo-spring-facade

---
+++ facade/Light.java
@@ -0,0 +1,6 @@
+package com.example.demo.facade;
+
+public interface Light {
+    void on();
+    void off();
+}
---
+++ facade/Main.java
@@ -0,0 +1,8 @@
+package com.example.demo.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        FacadePattern facadePattern = new FacadePattern();
+        facadePattern.lightOn();
+    }
+}
