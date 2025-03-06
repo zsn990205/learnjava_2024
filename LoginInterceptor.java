3e3d96d74e0150f1fff498c101a0e3c7491ff757
朱思楠 <zsn990205@163.com>
2025-03-06 10:04:16 +0800

    library-boot-book-interceptor

---
+++ LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.bite.book.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("目标方法执行前");
+        //true表示没有session就创建session
+        //false表示没有session直接退回
+        HttpSession session = request.getSession(false);
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("目标方法执行后");
+    }
+
+}
